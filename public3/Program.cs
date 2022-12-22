6a49a63ec9bfc377a9aa4fe9031435ad19bdeb9a
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-22 17:33:35 +0300

    домашняя работa задачи 1,2,3

---
+++ public3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число");
+int b =int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if(b>max) max = b;
+if(c>max) max = c;
