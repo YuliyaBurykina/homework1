ca8a6489fe586d49fa6758ff938c08a94da59727
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-22 15:24:31 +0300

    задача 2

---
+++ public2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[]Days = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье" };
+Console.Write("Задайте дни недели ->");
+int numberDays = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Days[numberDays-1]);
