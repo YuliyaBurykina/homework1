cb055d21074dd305d4750724547af0cb93956ad8
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-25 18:29:11 +0300

    задача 19

---
+++ задача 19/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите пятизначное число -> ");
+int num = int.Parse(Console.ReadLine()!);
+string strNum = $"{num}";
+
+if (strNum[0] == strNum[4] && strNum[1] == strNum[3])
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
