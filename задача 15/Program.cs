9072483a940de6ed7d3b81981389b14ddbd387b9
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-25 17:58:41 +0300

    задача 15

---
+++ задача 15/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+Console.Write("Задайте день недели -> ");
+int numberDays = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Days[numberDays - 1]);
+
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine(max);
