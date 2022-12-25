59652f5ed39c22d791033df024e7df28d57163fd
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-25 18:40:57 +0300

    задача 23

---
+++ задача 23/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("-> ");
+int N = int.Parse(Console.ReadLine()!);
+
+for (double i = 1; i <= N; i++) Console.WriteLine($"{i * i * i}");
