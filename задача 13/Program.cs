1e9c6afcb1afd9d431fc0d28cb7bf30d9c3d2878
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-25 17:11:34 +0300

    задача 13

---
+++ задача 13/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a/100 % 10;
+if (b % 10>0 )
+{
+    Console.Write("Третье число");
+    Console.WriteLine(b);
+}
+else {
+Console.WriteLine("Третьего числа нет");
+}
+
