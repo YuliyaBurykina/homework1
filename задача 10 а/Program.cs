89616436ee7aa9b5a22f68c6568c4ea6370f9227
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-25 18:03:36 +0300

    задача 10

---
+++ задача 10 а/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите трехзначное число ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a/ 10 % 10;
+Console.WriteLine(b);
+Console.WriteLine("мы получили вторую цифру от нужного числа");
