b8cfbc58fbbaca49e4704f087ab2e03bf15f06df
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-27 13:51:37 +0300

    задача 25

---
+++ задача 25/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число А и число В");
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+int index = 1;
+int result = 1;
+while (index <= B)
+{
+    result = result * A;
+    index = index + 1;
+}
+Console.WriteLine(result);
