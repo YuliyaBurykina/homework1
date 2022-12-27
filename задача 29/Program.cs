320f3bead3572b6d35fe6bc2956fd89f552ef486
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-27 14:30:59 +0300

    задача 28

---
+++ задача 29/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[]  array = new int[8];
+Console.Write("[");
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    string elements = Convert.ToString(array[i]);
+    int convert = Convert.ToInt32(elements);
+    Console.Write(convert.ToString() + ", ");
+}
+Console.Write("]");
