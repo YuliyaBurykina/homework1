bca98a6b0176a1cad33e3658a43f773b336892d0
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-31 10:30:19 +0300

    задача 64

---
+++ задача 64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+ Console.Write("Введите N: ");
+ int n = int.Parse(Console.ReadLine());
+ Console.WriteLine(PrintNumbers(n));
+
+ string PrintNumbers(int number)
+ {
+ if (number == 1) return "1";
+ string answer = number + " " + PrintNumbers(number - 1);
+ return answer; // "3 2 1"
+ }
