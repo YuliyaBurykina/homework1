c5a2ec461cd596064a750bac69be27a5495ca8c8
yuliyaBurykina <proskuryakova84@mail.ru>
2022-12-31 10:37:04 +0300

    задача 66

---
+++ задача 66/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+ Console.Write("Введите N: ");
+ int n = int.Parse(Console.ReadLine());
+ Console.WriteLine(SumNumbers(1, n));
+
+ int SumNumbers(int start, int end)
+ {
+ if (start == end) return end;
+ int answer = start + SumNumbers(start + 1, end);
+ return answer;
+ }
